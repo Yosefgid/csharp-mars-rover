281fec577294c3dec3913428fcee28d6bdde8a1b
Yosefgid <jossykasshun@yahoo.com>
2026-04-13 14:02:28 +0100

    Add comments to understand the task at hand

---
+++ MarsRover.Console/CompassDirection.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.ConsoleApp
+{
+    public enum CompassDirection
+    {   N,
+        E,
+        S,
+        W
+    }
+}
--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -1,4 +1,20 @@
-﻿namespace MarsRover.ConsoleApp
+﻿//Mission: Understand what needs to be done 
+//Rovers position and location represented by x y z
+//() => (x y z) where x, y is the co-ordinate of the x axis and y axis respectively and z is a cardinal direction
+//Plateau imagined as a rectangle with grids in a graph, where (x y z) are points in a graph
+//Rover receives inputs, () => i in (L R M) WHERE L is spin Left R is spin Right and M is Move forward
+//Input for starting position
+// second input a line of instruction to move the rover 
+// Plateau size defined as a max(x, y)
+
+//Task
+// 1.Spin Left
+// 2.Spin Right
+// 3.Move Forward by one grid point  
+
+
+
+namespace MarsRover.ConsoleApp
 {
     internal class Program
     {
