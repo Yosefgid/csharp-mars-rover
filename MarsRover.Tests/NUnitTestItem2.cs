85f58a4f7fb2b94ff090b6ea9bec820a94824c0d
Yosefgid <jossykasshun@yahoo.com>
2026-04-13 15:41:09 +0100

    test: add failing test for ParsrPosition

---
+++ MarsRover.Tests/NUnitTestItem2.cs
@@ -0,0 +1,15 @@
+﻿namespace MarsRover.Tests;
+
+public class NUnitTestItem2
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ MarsRover.Tests/ParsePosition_Test.cs
@@ -0,0 +1,22 @@
+﻿using MarsRover.ConsoleApp;
+using MarsRover.ConsoleApp.Parsers;
+
+namespace MarsRover.Tests;
+
+public class ParsePosition_Test
+{
+
+    [Test]
+    public void ParsePositions_ReturnsCorrectPositions_ForValidInputs()
+    {
+        //Arrange
+        var parser = new InputParser();
+        var input = "1 2 N";
+
+        //Act
+        var result = parser.ParsePositions(input);
+
+        //Assert
+        Assert.That(result, Is.EqualTo(new Position(1, 2, CompassDirection.N)));
+    }
+}
