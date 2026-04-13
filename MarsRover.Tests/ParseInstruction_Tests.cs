eb8ca0b0caf697f39459b6e07cf2321fe3f2abbc
Yosefgid <jossykasshun@yahoo.com>
2026-04-13 15:20:46 +0100

    test: add two passing tets for ParseInstruction

---
+++ MarsRover.Tests/ParseInstruction_Tests.cs
@@ -0,0 +1,58 @@
+using MarsRover.ConsoleApp;
+using MarsRover.ConsoleApp.Parsers;
+
+namespace MarsRover.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void ParseInstruction_ReturnsCorrectInstruction_ForTheRightInput()
+        {
+            //Arrange
+            var parser = new InputParser();
+            var input = "LRM";
+
+            //Act
+            var result = parser.ParseInstructions(input);
+
+            //Assert
+            Assert.That(result, Is.EqualTo(new List<Instruction>
+            {
+                Instruction.L,
+                Instruction.R,
+                Instruction.M
+            }));
+        }
+
+        [Test]
+        public void ParseInstruction_ThrowsException_ForInvalidInput()
+        {  
+            //Arrange
+            var parser = new InputParser();
+            var input = "LRX";
+
+          
+
+            //Assert/Act
+            Assert.Throws<ArgumentException>(() => parser.ParseInstructions(input));
+
+
+        }
+        [Test]
+        public void ParseInstruction_ReturnsEmptyList_FoEmptyInputString()
+        {
+            //Arrange
+            var parser = new InputParser();
+            var input = "";
+            //Act
+            var result = parser.ParseInstructions(input);
+
+
+
+            //Assert/Act
+            Assert.That(result, Is.Empty);
+
+
+        }
+    }
+}
